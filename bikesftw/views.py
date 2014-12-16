49d7bcaef65407a7dd38e400474762a06744f7af
arebe <rfboyce@gmail.com>
2014-12-15 23:31:49 -0500

    firt template

--- bikesftw/views.py
+++ bikesftw/views.py
@@ -1,3 +1,8 @@
 from django.shortcuts import render
+from .models import Bike
 
 # Create your views here.
+
+def bike_disp(request):
+	bikes = Bike.objects.all().order_by('date_created')
+	return render(request, 'bikesftw/bike_disp.html', {'bikes': bikes})
